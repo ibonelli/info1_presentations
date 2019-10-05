fab6c5e11a171cec3cead579b22794e6c1f70c64
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2019-10-05 16:17:24 -0300

    Mejor implementación de MD5

--- files/clase14/md5.c
+++ files/clase14/md5.c
@@ -1,49 +1,34 @@
 /*
  * Compile:
- * 		gcc -o md5 md5.c `libgcrypt-config --cflags --libs`
+ * 		gcc -o md5 md5.c -lssl -lcrypto
  */
 
-#include <gcrypt.h>
+#include <openssl/md5.h>
+#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-// Either GCRY_MD_MD5 or GCRY_MD_SHA1
-#define HASHV GCRY_MD_MD5
+#define MD5_HASH_SIZE 16
 
-int main(int argc, char **argv){
+int main()
+{
+        int n;
+        MD5_CTX c;
+        char buf[512];
+        ssize_t bytes;
+        unsigned char out[MD5_HASH_SIZE];
 
-  /* Test for arg string */
-  if ( argc < 2 ){
-    fprintf( stderr, "Usage: %s <string>\n", argv[0] );
-    exit( 1 );
-  }
+        MD5_Init(&c);
+        bytes=read(STDIN_FILENO, buf, 512);
+        while(bytes > 0)
+        {
+                MD5_Update(&c, buf, bytes);
+                bytes=read(STDIN_FILENO, buf, 512);
+        }
 
-  /* Length of message to encrypt */
-  int msg_len = strlen( argv[1] );
+        MD5_Final(out, &c);
 
-  /* Length of resulting sha1 hash - gcry_md_get_algo_dlen
-   * returns digest lenght for an algo */
-  int hash_len = gcry_md_get_algo_dlen( HASHV );
+        for(n=0; n<MD5_DIGEST_LENGTH; n++)
+                printf("%02x", out[n]);
 
-  /* output sha1 hash - this will be binary data */
-  unsigned char hash[ hash_len ];
-
-  /* output sha1 hash - converted to hex representation
-   * 2 hex digits for every byte + 1 for trailing \0 */
-  char *out = (char *) malloc( sizeof(char) * ((hash_len*2)+1) );
-  char *p = out;
-
-  /* calculate the SHA1 digest. This is a bit of a shortcut function
-   * most gcrypt operations require the creation of a handle, etc. */
-  gcry_md_hash_buffer( HASHV, hash, argv[1], msg_len );
-
-  /* Convert each byte to its 2 digit ascii
-   * hex representation and place in out */
-  int i;
-  for ( i = 0; i < hash_len; i++, p += 2 ) {
-    snprintf ( p, 3, "%02x", hash[i] );
-  }
-
-  printf( "%s\n", out );
-  free( out );
+        return(0);        
 }
