5902b60d57d1efb4394aa17382888cb8c701c2a0
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2020-07-04 21:27:42 -0300

    Cambios de clase.

--- files/clase07/RECUR3b_Factorial_RecursivoStack-MasConsumo.c
+++ files/clase07/RECUR3b_Factorial_RecursivoStack-MasConsumo.c
@@ -1,32 +1,39 @@
-/*    Calcula el factorial de un entero N     */
-/*      utilizando una funcion recursiva.     */
+/* Calcula el factorial de un entero N
+ *        utilizando una funcion recursiva.
+ *
+ * Debemos compilarlo sin optimización para
+ * que reserve la memoria del stack/pila en
+ * factorial():
+ *        gcc -O0 -o ej ej-fact-stack.c
+ */
 
 #include <stdio.h>
 
-#define MAX 100
+#define MAX 1000000
 
-unsigned long int FACT(int N);
+unsigned long int factorial(int N);
 
-main()
+int main(void)
 {
-  char c;
-  int N;
-  do {
-    printf("\nIngrese un numero: ");
-    scanf("%d",&N);
-    fflush(stdin);
-    printf("\n\t%12d\t\t  %12lu\n",N,FACT(N));
-    printf("\n\nOtro numero? (s/n): ");
-    c=getchar();
-  } while(c=='s');
+	char c;
+	int N;
+	do {
+		printf("\nIngrese un numero: ");
+		scanf("%d",&N);
+		fflush(stdin);
+		printf("\n\t%12d\t\t	%12lu\n",N,factorial(N));
+		printf("\n\nOtro numero? (s/n): ");
+		// Hacemos un segundo getchar para tomar el ENTER
+		getchar();
+		c=getchar();
+	} while(c=='s');
 }
 
-unsigned long int FACT(int N)   /*  FUNCION RECURSIVA  */
+unsigned long int factorial(int N)	 /*	FUNCION RECURSIVA	*/
 {
-  char vec[MAX], i; /* Gastamos memoria de la pila */
-  for(i=0;i<MAX;i++) vec[i]=i; /* Para que realmente la tome, hay que asignarla */
-  printf("\n %d \t %p ",N,&N);
-  
-  if (!N) return 1 ;
-  return N * FACT(N-1);
+	char vec[MAX], i; /* Tomamos memoria de la pila */
+	printf("\n %d \t %p ",N,&N);
+
+	if (!N) return 1 ;
+	return N * factorial(N-1);
 }
