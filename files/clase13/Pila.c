308f37fb7714bdc287e8a95c11f9a5d4df50d529
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2020-09-12 20:54:46 -0300

    Making the examples work on Linux 100%.

--- files/clase13/Pila.c
+++ files/clase13/Pila.c
@@ -1,107 +1,105 @@
+/*
+ * Para compilar usar:
+ * 			gcc -o ej Pila.c -lcurses
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
-#define MAX 5
+#include <ctype.h>
+#include <ncurses.h>
 
-void ingresa(int *pr,float **d) {
-	char k;
-	float dato;
+#define MAX 5
+#define STRINT 10
+
+void ingresa(int *pr, int *d);
+void lee(int *pr, int *d);
+void lista(int pr, int *d);
+void press_key(char *msg);
+
+void main(void) {
+	int	co=0, i;
+	int da[MAX];
+	char op[STRINT];
+
+	do {
+		system("clear");
+		printf("1 - Ingresa datos\n");
+		printf("2 - Lee datos\n");
+		printf("3 - Lista datos\n");
+		printf("4 - Salir\n\n");
+		printf("Ingrese una opcion ( 1 - 4 ) : ");
+		setbuf(stdin, NULL);
+		fgets(op,STRINT,stdin);
+		i=atoi(op);
+		switch(i) {
+			case 1:
+				ingresa(&co,da);
+				break;
+			case 2:
+				lee(&co,da);
+				break;
+			case 3:
+				lista(co,da);
+				break;
+			case 4:
+				exit(0);
+		}
+	} while(i!=4);
+}
+
+void ingresa(int *pr, int *d) {
+	char k, dato[STRINT];
 	do {
 		if(*pr>=MAX) {
-			clrscr();
-			printf("\n\nPila llena");
-			printf("\n\nPresione una tecla para continuar\n");
-			getche();
+			system("clear");
+			press_key("\nPila llena\nPresione una tecla para continuar\n");
 			return;
-		} else {
-			d[*pr]=(float *)malloc(3000*sizeof(float));
-			if(d[*pr]==NULL) {
-				clrscr();
-				printf("\n\nNo hay memoria disponible\n");
-				printf("\n\nPresione una tecla para continuar\n");
-				getch();
-				return;
-			} else {
-				printf("\nIngrese el dato : ");
-				scanf("%f",&dato);
-				*d[*pr]=dato;
-				(*pr)++;
-			}
 		}
+		printf("\nIngrese el dato : ");
+		fgets(dato,STRINT,stdin);
+		d[*pr]=atoi(dato);
+		(*pr)++;
 		printf("\nOtro dato ( S / N )? ");
-		k=toupper(getche());
+		fgets(dato,STRINT,stdin);
+		k = toupper(dato[0]);
 	} while(k=='S');
 }
 
-void lee(int *pr,float **d) {
-	char k;
+void lee(int *pr, int *d) {
+	char k, dato[STRINT];
 	do {
 		(*pr)--;
-		if(*pr<0)
-				{
-					clrscr();
-					printf("\n\nPila vacia");
-					printf("\n\nPresione una tecla para continuar\n");
-					(*pr)=0;
-					getch();
-					return;
-				}
-			else
-				{
-					if(d[*pr]==NULL)
-							{
-								printf("No hay dato\n");
-								printf("\n\nPresione una tecla para continuar\n");
-								getch();
-								k='N';
-							}
-						else
-							{
-								printf("\nEl dato es : %f",*d[*pr]);
-								free(d[*pr]);
-								d[*pr]=NULL;
-							}
-				}
+		if(*pr<0) {
+			(*pr)=0;
+			system("clear");
+			press_key("\nPila vacia\nPresione una tecla para continuar\n");
+			return;
+		}
+		printf("\nEl dato es : %d",d[*pr]);
 		printf("\nOtro dato ( S / N )? ");
-		k=toupper(getche());
+		fgets(dato,STRINT,stdin);
+		k = toupper(dato[0]);
 	} while(k=='S');
 }
 
-void lista(int *pr,float **d)
-	{
-		int i;
-		clrscr();
-		for(i=*pr-1;i>=0;i--)
-			printf("%f\t",*d[i]);
-		printf("\n\nPresione una tecla para continuar");
-		getch();
-	}
+void lista(int pr, int *d) {
+	int i;
+	system("clear");
+	for(i=pr-1;i>=0;i--)
+		printf("%d\t",d[i]);
+	printf("\n\nPresione una tecla para continuar");
+	getchar();
+}
 
-void main(void)
-	{
-		int	co=0,i;
-		float	*da[MAX];
-		for(i=0;i<MAX;i++)
-			da[i]=NULL;
-		do
-			{
-				clrscr();
-				printf("1 - Ingresa datos\n");
-				printf("2 - Lee datos\n");
-				printf("3 - Lista datos\n");
-				printf("4 - Salir\n\n");
-				printf("Ingrese una opcion ( 1 - 4 ) : ");
-				scanf("%d",&i);
-				switch(i)
-					{
-							case	1:	ingresa(&co,da);
-												break;
-							case	2:	lee(&co,da);
-												break;
-							case	3:	lista(&co,da);
-												break;
-							case	4:	exit(0);
-					}
-			}
-		while(i!=4);
-	}
+void press_key(char *msg) {
+	char input;
+
+	initscr(); // entering ncurses mode
+	raw();	 // CTRL-C and others do not generate signals
+	noecho();  // pressed symbols wont be printed to screen
+	cbreak();  // disable line buffering
+	erase();
+	mvprintw(1,0, msg);
+	input = getch();
+	endwin(); // leaving ncurses mode
+}
